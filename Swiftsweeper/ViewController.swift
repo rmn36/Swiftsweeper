2dc722e63e8c963b46c67cd1dea585f6427d2fb9
Ryan Nowacoski <rmn36@case.edu>
2014-11-26 19:49:16 -0500

    Initial Commit

---
+++ Swiftsweeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiftsweeper
+//
+//  Created by Ryan Nowacoski on 11/26/14.
+//  Copyright (c) 2014 Ryan Nowacoski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
