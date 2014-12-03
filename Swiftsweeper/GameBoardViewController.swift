42b0329f321344a79cfac6e095f7c36f8b236c0f
Ryan Nowacoski <rmn36@case.edu>
2014-12-03 15:04:02 -0500

    Add GameBoardViewController
    Signed-off-by: Ryan Nowacoski <rmn36@case.edu>

--- Swiftsweeper/GameBoardViewController.swift
+++ Swiftsweeper/GameBoardViewController.swift
@@ -1,24 +1,30 @@
 //
-//  ViewController.swift
+//  GameBoardViewController.swift
 //  Swiftsweeper
 //
-//  Created by Ryan Nowacoski on 11/26/14.
+//  Created by Ryan Nowacoski on 12/3/14.
 //  Copyright (c) 2014 Ryan Nowacoski. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class GameBoardViewController: UIViewController {
+    @IBOutlet weak var boardView: UIView!
+    @IBOutlet weak var movesLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func newGamePressed() {
+        println("new game");
+    }
 }
-
